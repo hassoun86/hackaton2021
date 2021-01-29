0b426e51800cf4abc0c928391d7512aca4899607
hassoun86 <husseinfarhat1@hotmail.com>
2021-01-29 15:51:36 +0100

    map

--- src/App/components/LandingPage.tsx
+++ src/App/components/LandingPage.tsx
@@ -6,16 +6,16 @@ import { Link } from 'react-router-dom';
 
 export const LandingPage: FC = () => {
   return (
-      <Fragment>
-          <div className="landing-image">
-              <img src="/assets/ubahn-d2.svg" />
-          </div>
-          <div className="mt-5 text-center">
-              <h1 className="f-36">Ride Share</h1>
-          </div>
-          <div className="mt-5 text-center">
-          <Link to="/select-rider"><Button variant="primary">Let's Start</Button></Link>
-          </div>
-      </Fragment>
+    <Fragment>
+      <div className="landing-image">
+        <img src="/assets/ubahn-d2.svg" />
+      </div>
+      <div className="mt-5 text-center">
+        <h1 className="f-36">Share a Ride</h1>
+      </div>
+      <div className="mt-5 text-center">
+        <Link to="/select-rider"><Button variant="primary">Let's Start</Button></Link>
+      </div>
+    </Fragment>
   );
 };
--- src/App/index.tsx
+++ src/App/index.tsx
@@ -4,22 +4,26 @@ import 'assets/main.scss';
 import { LandingPage } from './components/LandingPage';
 import { SelectRider } from './components/SelectRider';
 import { SearchDestination } from './marcin/SearchDestination';
+import { MapPage } from './components/MapPage';
 
 export const App: React.FC = () => {
   return (
-        <Switch>
-          <Route path="/select-rider">
-            <SelectRider />
-          </Route>
-          <Route path="/search-destination">
-            <SearchDestination />
-          </Route>
-          <Route path="/other">
-            <div>Other page</div>
-          </Route>
-          <Route path="/">
-            <LandingPage/>
-          </Route>
-        </Switch>
+    <Switch>
+      <Route path="/select-rider">
+        <SelectRider />
+      </Route>
+      <Route path="/search-destination">
+        <SearchDestination />
+      </Route>
+      <Route path="/map">
+        <MapPage />
+      </Route>
+      <Route path="/other">
+        <div>Other page</div>
+      </Route>
+      <Route path="/">
+        <LandingPage />
+      </Route>
+    </Switch>
   );
 };
